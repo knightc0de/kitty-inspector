fd60b051408f2277a68cc8aac0f3e58dd92ca2b3
knightc0de <its_puneet143@proton.me>
2026-06-25 01:28:43 +0530

    some error are now fixed !python -u

--- kitty_investigator.py
+++ kitty_investigator.py
@@ -1,6 +1,5 @@
 import struct 
 from  pathlib import Path 
-import magic 
 import sys 
 import re 
 
@@ -16,29 +15,43 @@ class Kitty_investigator():
             self.results = results.copy()
     
       def file_type(self):
-          try:
-               m = magic.Magic(mime=True)
-               ftype = m.from_file(str(self.path))
-               ftype = ftype.split(",")[0].strip()
-               self.results["file_type"] = ftype
-               ext = Path(self.path).suffix.lower()
-               if ext == ".py":
-                      self.results["file_type"] = "Python Script"
-               elif ext == ".c":
-                      self.results["file_type"] = "C Source File"
-               elif ext in [".cpp", ".cc", ".cxx"]:
-                      self.results["file_type"] = "C++ Source File"
-               elif ext in [".sh",".bash",".zsh"]:
-                      self.results["file_type"] = "Shell Script"
-               elif ext == ".html" or ext == ".htm":
-                      self.results["file_type"] = "HTML Document"
-               elif ext == ".php":
-                      self.results["file_type"] = "PHP Script"
-               elif ext == ".js":
-                     self.results["file_type"] = "JavaScript File"
-               elif ext == ".java":
-                     self.results["file_type"] = "Java Source File"
-             
-          except Exception:
-              self.results["file type"] = "Unknown (magic unavailable)"
+        try:
+            ext = self.path.suffix.lower()
 
+            if ext == ".py":
+                self.results["file_type"] = "Python Script"
+
+            elif ext == ".c":
+                self.results["file_type"] = "C Source File"
+
+            elif ext in [".cpp", ".cc", ".cxx"]:
+                self.results["file_type"] = "C++ Source File"
+
+            elif ext in [".sh", ".bash", ".zsh"]:
+                self.results["file_type"] = "Shell Script"
+
+            elif ext in [".html", ".htm"]:
+                self.results["file_type"] = "HTML Document"
+
+            elif ext == ".php":
+                self.results["file_type"] = "PHP Script"
+
+            elif ext == ".js":
+                self.results["file_type"] = "JavaScript File"
+
+            elif ext == ".java":
+                self.results["file_type"] = "Java Source File"
+
+            else:
+                self.results["file_type"] = f"Unknown ({ext})"
+
+        except Exception as e:
+            print(f"ERROR: {e}")
+            self.results["file_type"] = "Unknown"
+    
+        return self.results
+
+
+kitty = Kitty_investigator("req.py")
+kitty = kitty.file_type()
+print(kitty)
